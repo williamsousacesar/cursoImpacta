3a22eede99586cd973810507c5d4909841eba799
William de Sousa Cesar <william.cesar@b2wdigital.com>
2023-01-17 21:06:18 -0300

    Curso Impacta 17012023

---
+++ JAVA WEB/appCrudProgramador/src/main/java/appCrudProgramador/appCrudProgramador/AppCrudProgramadorApplication.java
@@ -0,0 +1,13 @@
+package appCrudProgramador.appCrudProgramador;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppCrudProgramadorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppCrudProgramadorApplication.class, args);
+	}
+
+}
