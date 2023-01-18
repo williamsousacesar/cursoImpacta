cc64828db531062119b028db00d07a4c47acf4c9
William de Sousa Cesar <william.cesar@b2wdigital.com>
2023-01-18 19:31:49 -0300

    .

---
+++ appCrudProgramador/src/test/java/appCrudProgramador/appCrudProgramador/AppCrudProgramadorApplicationTests.java
@@ -0,0 +1,13 @@
+package appCrudProgramador.appCrudProgramador;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppCrudProgramadorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
