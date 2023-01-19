b97e0990e2d4a0659b9b3ba9b7ff385b4cbd51f1
William de Sousa Cesar <william.cesar@b2wdigital.com>
2023-01-18 23:14:42 -0300

    .

--- cadastroProfessor/src/main/java/cadastroProfessor/cadastroProfessor/controller/TurmaController.java
+++ cadastroProfessor/src/main/java/cadastroProfessor/cadastroProfessor/controller/TurmaController.java
@@ -4,8 +4,10 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -25,6 +27,43 @@ public class TurmaController {
 	
 	@Autowired
 	TurmaRepository turmaRepository;
+
+	@GetMapping("/listarTurma")
+	public List<Turma> listarTurma() {
+		List<Turma> turma =   (List<Turma>) turmaRepository.findAll();
+		
+		return turma;
+	}
+	
+	@PostMapping("/cadastraTurma")
+	public void cadastrarTurma (@RequestBody Turma turma) {
+		
+		turmaRepository.save(turma);
+	
+	}
+	
+	@GetMapping("/turma/{codigo}")
+	public Turma procurarTurma(@PathVariable long codigo) {
+		
+		Turma turma = turmaRepository.findById(codigo);
+		
+		return turma;
+	}
+	
+	@DeleteMapping("/turmaDeletar/{codigo}")
+	public void deletarTurma(@PathVariable long codigo) {
+		Turma turma = turmaRepository.findById(codigo);
+		
+		turmaRepository.delete(turma);
+		
+	}
+	
+	@PutMapping("/editarTurma")
+	public void editarTurma(@RequestBody Turma turma) {
+		
+		turmaRepository.save(turma);
+		
+	}
 	
 	@PutMapping("/cadastrarTurma/{codigo}")
 	public void cadastrarTurma (@RequestBody Turma turma,  @PathVariable long codigo) {
@@ -33,16 +72,5 @@ public class TurmaController {
 		turmaRepository.save(turma);
 		
 	}
-	
-	@GetMapping("/listarTurma")
-	public List<Turma> listarTurma() {
-		List<Turma> turma =   (List<Turma>) turmaRepository.findAll();
-		
-		return turma;
-	}
-	
-	
-	
-
 
 }
--- cadastroProfessor/src/main/java/cadastroProfessor/cadastroProfessor/repository/TurmaRepository.java
+++ cadastroProfessor/src/main/java/cadastroProfessor/cadastroProfessor/repository/TurmaRepository.java
@@ -2,8 +2,11 @@ package cadastroProfessor.cadastroProfessor.repository;
 
 import org.springframework.data.repository.CrudRepository;
 
+import cadastroProfessor.cadastroProfessor.model.Professor;
 import cadastroProfessor.cadastroProfessor.model.Turma;
 
 public interface TurmaRepository extends CrudRepository<Turma, Long> {
+	
+	Turma findById(long codigo);
 
 }
